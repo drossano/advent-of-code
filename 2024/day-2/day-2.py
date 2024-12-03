8f4a7718ddca4f1387cb3e9c90cdec7215b0fbee
Dean <72749793+drossano@users.noreply.github.com>
2024-12-02 21:59:02 -0500

    Solve part 2

--- 2024/day-2/day-2.py
+++ 2024/day-2/day-2.py
@@ -12,86 +12,31 @@ def get_reports(filename):
 def inc_or_dec(report):
   return sorted(report) == report or sorted(report, reverse=True) == report
 
-def inc_or_dec_dampener(report):
-  changes = np.diff(report)
-  negs = 0
-  pos = 0
-  for i in range(len(changes)):
-    if changes[i] > 0:
-      pos += 1
-    else:
-      negs += 1
-  if pos > negs:
-      if changes[-1] < 0:
-        del report[-1]
-        return report
-      else:
-        for i in range(len(changes)):
-          if changes[i] < 0:
-              del report[i]
-              return report
-  else:
-    if changes[-1] > 0:
-      del report[-1]
-      return report
-    for i in range(len(changes)):
-      if changes[i] > 0:
-          del report[i]
-          return report
-
-
-
 def is_level_change_safe(report):
   changes = np.abs(np.diff(report))
   safe = all(change >= 1 for change in changes) and all(change <= 3 for change in changes)
   return safe
 
-def change_dampener(report):
-  changes = np.abs(np.diff(report))
-  if changes[0] < 1 or changes [0]> 3:
-    del report[0]
-    return report
-  elif changes[-1] < 1 or changes[-1] > 3:
-    del report[-1]
-    return report
-  else:
-    dupes = [k for k, v in Counter(report).items() if v >1]
-    if len(dupes) > 1 or not dupes:
-      return  False
+def dampcheck(report):
+  `thank you worf!`
+  for count, ele in enumerate(report):
+    report.pop(count)
+    if inc_or_dec(report) and is_level_change_safe(report):
+      return True
     else:
-      report.remove(dupes[0])
-      return report
+      report.insert(count, ele)
+  return False
 
 def main():
   safe_reports = 0
-  safe_report_list = []
-  reports = get_reports('example.txt')
+  reports = get_reports('input.txt')
   for report in reports:
-    if inc_or_dec(report) == True:
-      if is_level_change_safe(report) == True:
+    if inc_or_dec(report) and is_level_change_safe(report):
         safe_reports += 1
-        safe_report_list.append(report)
-      else:
-        dampened_report = change_dampener(report)
-        if dampened_report != False:
-          if is_level_change_safe(dampened_report) == True:
-            safe_reports +=1
-            safe_report_list.append(report)
-    else:
-      dampened_report = inc_or_dec_dampener(report)
-      if inc_or_dec(dampened_report) == True:
-        if is_level_change_safe(dampened_report) == True:
-          safe_reports += 1
-          safe_report_list.append(report)
+    elif dampcheck(report):
+      safe_reports += 1
+
 
   print(safe_reports)
-  print(safe_report_list)
 
 main()
-
-#part 2
-# if inc or dec fails
-# inc or dec passes
-  # change fails
-  # should only matter for matches?
-  # just remove dupes???
