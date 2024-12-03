36e93d7484141ed052ac9ad79550cdcbd98a62a9
Dean <72749793+drossano@users.noreply.github.com>
2024-12-03 13:58:16 -0500

    remove comments

--- 2024/day-3/day-3.py
+++ 2024/day-3/day-3.py
@@ -1,17 +1,10 @@
 import regex as re
 
-# Part 1 goal is to get total of muls
-# a mul is OMLY mul(INT,INT)
-# scan through text and retrieve ints in the right sequence as tuples? if dont shows up ignore until do
-
-#   scan throuh text
-
-
 def get_memory(filename):
   with open(filename) as f:
     memory = f.read()
     return memory
-#   find a muls(INT,INT) sequence, don'ts and dos
+
 def find_instructions(corrupted_memory):
   instructions = re.findall(r"mul\(\d+,\d+\)|don't\(\)|do\(\)", corrupted_memory)
   return instructions
@@ -29,20 +22,12 @@ def get_factors_from_instructions(instructions):
         factors.append(find_factors(instruction))
   return factors
 
-    
-
 def find_factors(mul_text):
   mul = re.findall(r"[m][u][l][(](\d+)[,](\d+)[)]", mul_text)
   return mul[0]
 
-
-  
-# Multilply each two tuple
 def multiply_factors(factors):
   return int(factors[0]) * int(factors[1])
-
-
-# Add results
 
 def main ():
   total = 0
