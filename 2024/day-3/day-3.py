3bf5f566446ea64d65e7ad00f60b494951152519
Dean <72749793+drossano@users.noreply.github.com>
2024-12-03 06:49:02 -0500

    Write out steps

---
+++ 2024/day-3/day-3.py
@@ -0,0 +1,14 @@
+import regex as re
+
+# Part 1 goal is to get total of muls
+# a mul is OMLY mul(INT,INT)
+# scan through text and retrieve ints in the right sequence as tuples?
+#   scan throuh text
+#   find a mul(INT,INT) sequence
+#   add ints to a two tuple
+
+# Multilply each two tuple
+# Add results
+
+def main ():
+  result = 0
