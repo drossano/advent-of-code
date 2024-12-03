0ec50823d398a1832bb0c7cd400be8cb51ca8033
Dean <72749793+drossano@users.noreply.github.com>
2024-12-03 07:05:27 -0500

    add regex block

--- 2024/day-3/day-3.py
+++ 2024/day-3/day-3.py
@@ -4,11 +4,21 @@ import regex as re
 # a mul is OMLY mul(INT,INT)
 # scan through text and retrieve ints in the right sequence as tuples?
 #   scan throuh text
+
+def get_memory(filename):
+  with open(filename) as f:
+    memory = f.read()
+    return memory
 #   find a mul(INT,INT) sequence
+def find_muls(memory):
+  #"[m][u][l][(]\d+[,]\d+[)]"gm
 #   add ints to a two tuple
 
 # Multilply each two tuple
 # Add results
 
 def main ():
-  result = 0
+  print(type(get_memory('example.txt')))
+        
+
+main()
