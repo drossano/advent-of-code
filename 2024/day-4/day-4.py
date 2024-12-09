0203f649e1c7aef7d71cd1d7f2a9910b80023701
Dean <72749793+drossano@users.noreply.github.com>
2024-12-09 15:18:15 -0500

    Solve Part 1

--- 2024/day-4/day-4.py
+++ 2024/day-4/day-4.py
@@ -16,27 +16,43 @@ def get_diags(word_search, direction):
 
     # start from line 0
   k = 0
-  for k in range(2 * (max_length - 1)):
+  for k in range(2 * (max_length)):
   # while k <= 2 * (max_length - 1):
     line = []
     y = lengths['y'] - 1
-    while  y >= 0:
-      if direction == 'ur':
-        x = k - lengths['y'] - y
-      elif direction == 'dl': 
-        x = k - y
-      if x >= 0 and x < lengths['x']:
-        line.append(word_search[y][x])
-      y -= 1
-    if len(line) > 0:
+    if direction == 'ur': 
+        y = lengths['y'] - 1
+        while y >=0:
+          x = k - y
+          if x >= 0 and x < lengths['x']:
+            line.append(word_search[y][x])
+          y -= 1
+    if direction == 'dl':
+        y = lengths['y'] -1
+        while y >= 0:
+          x =  k - (lengths['y'] - y)
+          if x >= 0 and x < lengths['x']:
+            line.append(word_search[y][x])
+          y -=1
+    if len(line) >= 4:
       diags.append(line)
   return diags
+
+def find_xmas(lines):
+  xmases = 0
+  for line in lines:
+    for index in range(len(line)):
+      word = "".join(line[index : index + 4])
+      if word == 'XMAS' or word == 'SAMX':
+        xmases += 1
+  return xmases
  
 def main ():
-  word_search = get_word_search('example.txt')
+  word_search = get_word_search('input.txt')
   verticals = get_verticals(word_search)
-  ur_diags = get_diags(word_search, 'dl')
-  print(ur_diags)
-
+  ur_diags = get_diags(word_search, 'ur')
+  dl_diags = get_diags(word_search, 'dl')
+  xmases = find_xmas(word_search) + find_xmas(verticals) + find_xmas(ur_diags) + find_xmas(dl_diags)
+  print(xmases)
 
 main()
